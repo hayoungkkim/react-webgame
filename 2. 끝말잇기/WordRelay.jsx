ff22f4dd7b44911461bc231b98edccee8e488add
202113 <202113@ssg.com>
2021-11-10 14:02:46 +0900

    2-10. 끝말잇기 Hooks로 전환하기

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -1,52 +1,40 @@
 const React = require("react");
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-	state = {
-		word: "김하영",
-		value: "",
-		result: "",
-	};
+const WordRelay = () => {
+	const [word, setWord] = useState("김하영");
+	const [value, setValue] = useState("");
+	const [result, setResult] = useState("");
+	const inputRef = useRef(null);
 
-	onSubmit = (e) => {
+	const onSubmit = (e) => {
 		e.preventDefault();
-		if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-			this.setState({
-				result: "딩동댕",
-				word: this.state.value,
-				value: "",
-			});
+		if (word[word.length - 1] === value[0]) {
+			setResult("딩동댕");
+			setWord(value);
+			setValue("");
 		} else {
-			this.setState({
-				result: "땡",
-				value: "",
-			});
+			setResult("땡");
+			setValue("");
 		}
-		this.input.focus();
+		inputRef.current.focus();
 	};
 
-	onChange = (e) => {
-		this.setState({ value: e.target.value });
+	const onChange = (e) => {
+		setValue(e.target.value);
 	};
 
-	input;
-
-	onRefInput = (c) => {
-		this.input = c;
-	};
-
-	render() {
-		return (
-			<>
-				<div>{this.state.word}</div>
-				<form onSubmit={this.onSubmit}>
-					<input ref={this.onRefInput} type="text" value={this.state.value} onChange={this.onChange} />
-					<button>입력!</button>
-				</form>
-				<div>{this.state.result}</div>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<div>{word}</div>
+			<form onSubmit={onSubmit}>
+				<label htmlFor="wordInput">글자를 입력하세요.</label>
+				<input id="wordInput" className="wordInput" ref={inputRef} type="text" value={value} onChange={onChange} />
+				<button>입력!</button>
+			</form>
+			<div>{result}</div>
+		</>
+	);
+};
 
 module.exports = WordRelay;
