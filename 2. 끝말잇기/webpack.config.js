6be5fbd8807d574418a39293096fa8bc5cc4eb48
202113 <202113@ssg.com>
2021-11-10 13:07:13 +0900

    2-8. 끝말잇기 Class 만들기

--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -11,19 +11,30 @@ module.exports = {
 	entry: {
 		app: ["./client"],
 	}, // 입력
-
 	module: {
 		rules: [
 			{
 				test: /\.jsx?/,
 				loader: "babel-loader",
 				options: {
-					presets: ["@babel/preset-env", "@babel/preset-react"],
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								targets: {
+									browsers: ["> 1% in KR"],
+								},
+								debug: true,
+							},
+						],
+						"@babel/preset-react",
+					],
+					plugins: [],
 				},
 			},
 		],
 	},
-
+	plugins: [],
 	output: {
 		path: path.join(__dirname, "dist"),
 		filename: "app.js",
