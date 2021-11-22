fdc3e4a309deb8ed40332dbc28a3facce09fc790
hayoungkkim <glorious_3@naver.com>
2021-11-22 16:28:47 +0900

    4-2. setTimeout 넣어 반응속도체크

--- 4.반응속도체크/ResponseCheckClass.jsx
+++ 4.반응속도체크/ResponseCheckClass.jsx
@@ -7,11 +7,46 @@ class ResponseCheck extends Component {
 		result: [],
 	};
 
-	onClickScreen = () => {};
+	timeout;
+	startTime;
+	endTime;
+
+	onClickScreen = () => {
+		const { state, message, result } = this.state;
+		if (state === "waiting") {
+			this.setState({
+				state: "ready",
+				message: "초록색이 되면 클릭하세요.",
+			});
+			this.timeout = setTimeout(() => {
+				this.setState({
+					state: "now",
+					message: "지금 클릭",
+				});
+				this.startTime = new Date();
+			}, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
+		} else if (state === "ready") {
+			clearTimeout(this.timeout);
+			this.setState({
+				state: "waiting",
+				message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요.",
+			});
+		} else if (state === "now") {
+			// 반응속도 체크
+			this.endTime = new Date();
+			this.setState((prevState) => {
+				return {
+					state: "waiting",
+					message: "클릭해서 시작하세요.",
+					result: [...prevState.result, this.endTime - this.startTime],
+				};
+			});
+		}
+	};
 
 	renderAverage = () => {
 		const { result } = this.state;
-		result.length === 0 ? null : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>;
+		return result.length === 0 ? null : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>;
 	};
 
 	render() {
