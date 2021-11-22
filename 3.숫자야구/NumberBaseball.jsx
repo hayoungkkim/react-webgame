61e4c8c343ab00e0f10f141d4c68e4c33e5a9e08
hayoungkkim <glorious_3@naver.com>
2021-11-22 14:38:57 +0900

    3-12. React.createRef

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Try from "./Try";
 
 // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -17,6 +17,7 @@ const NumberBaseball = () => {
 	const [value, setValue] = useState("");
 	const [answer, setAnswer] = useState(getNumbers());
 	const [tries, setTries] = useState([]);
+	const inputEl = useRef(null);
 
 	const onSubmitForm = (e) => {
 		e.preventDefault();
@@ -29,6 +30,7 @@ const NumberBaseball = () => {
 			setValue("");
 			setAnswer(getNumbers());
 			setTries([]);
+			inputEl.current.focus();
 		} else {
 			const answerArray = value.split("").map((v) => parseInt(v));
 			let strike = 0;
@@ -39,6 +41,7 @@ const NumberBaseball = () => {
 				setValue("");
 				setAnswer(getNumbers());
 				setTries([]);
+				inputEl.current.focus();
 			} else {
 				for (let i = 0; i < 4; i += 1) {
 					if (answerArray[i] === answer[i]) {
@@ -51,6 +54,7 @@ const NumberBaseball = () => {
 					return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` }];
 				});
 				setValue("");
+				inputEl.current.focus();
 			}
 		}
 	};
@@ -63,7 +67,7 @@ const NumberBaseball = () => {
 		<>
 			<h1>{result}</h1>
 			<form onSubmit={onSubmitForm}>
-				<input maxLength={4} value={value} onChange={onChangeInput} />
+				<input ref={inputEl} maxLength={4} value={value} onChange={onChangeInput} />
 			</form>
 			<div>시도: {tries.length}</div>
 			<ul>
