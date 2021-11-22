84a748a1fdb9f07a9e877af071514da774b43567
hayoungkkim <glorious_3@naver.com>
2021-11-22 12:10:50 +0900

    3-3. 리액트 반복문(key)

--- 3.숫자야구/NumberBaseball.jsx
+++ 3.숫자야구/NumberBaseball.jsx
@@ -28,8 +28,20 @@ class NumberBaseball extends Component {
 				</form>
 				<div>시도: {tries.length}</div>
 				<ul>
-					{["사과", "바나나", "포도", "귤", "감", "배", "밤"].map((v, i) => {
-						return <li>{v}</li>;
+					{[
+						{ fruit: "사과", taste: "맛있다" },
+						{ fruit: "바나나", taste: "맛없다" },
+						{ fruit: "포도", taste: "시다" },
+						{ fruit: "귤", taste: "시다" },
+						{ fruit: "감", taste: "시다" },
+						{ fruit: "배", taste: "시다" },
+						{ fruit: "밤", taste: "시다" },
+					].map((v, i) => {
+						return (
+							<li key={v.fruit + v.taste}>
+								<b>{v.fruit}</b> - {v.taste}
+							</li>
+						);
 					})}
 				</ul>
 			</>
