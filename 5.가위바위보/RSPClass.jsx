617025c30a8e18dd8c573d7639c4e79cd1bdb512
hayoungkkim <glorious_3@naver.com>
2021-12-03 11:48:17 +0900

    5-4. 고차 함수와 Q&A

--- 5.가위바위보/RSPClass.jsx
+++ 5.가위바위보/RSPClass.jsx
@@ -58,7 +58,7 @@ class RSP extends Component {
 		}
 	};
 
-	onClickBtn = (choice) => {
+	onClickBtn = (choice) => () => {
 		const { imgCoord } = this.state;
 		clearInterval(this.interval);
 		const myScore = scores[choice];
@@ -94,13 +94,13 @@ class RSP extends Component {
 			<>
 				<div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
 				<div>
-					<button id="rock" className="btn" onClick={() => this.onClickBtn("바위")}>
+					<button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
 						바위
 					</button>
-					<button id="scissor" className="btn" onClick={() => this.onClickBtn("가위")}>
+					<button id="scissor" className="btn" onClick={this.onClickBtn("가위")}>
 						가위
 					</button>
-					<button id="paper" className="btn" onClick={() => this.onClickBtn("보")}>
+					<button id="paper" className="btn" onClick={this.onClickBtn("보")}>
 						보
 					</button>
 				</div>
