e58f31f34d123adf520a805b4b557f7329642d15
hayoungkkim <glorious_3@naver.com>
2021-12-15 09:30:11 +0900

    6-3. componentDidUpdate

--- 6.로또/LottoClass.jsx
+++ 6.로또/LottoClass.jsx
@@ -25,7 +25,8 @@ class Lotto extends Component {
 
 	timeouts = [];
 
-	componentDidMount() {
+	runTimeouts = () => {
+		console.log("runTimeouts");
 		const { winNumbers } = this.state;
 		for (let i = 0; i < winNumbers.length - 1; i++) {
 			this.timeouts[i] = setTimeout(() => {
@@ -42,6 +43,21 @@ class Lotto extends Component {
 				redo: true,
 			});
 		}, 7000);
+	};
+
+	componentDidMount() {
+		console.log("didMount");
+		this.runTimeouts();
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log("didUpdate");
+		if (this.state.winBalls.length === 0) {
+			this.runTimeouts();
+		}
+		if (prevState.winNumbers !== this.state.winNumbers) {
+			console.log("로또 숫자를 생성합니다.");
+		}
 	}
 
 	componentWillUnmount() {
